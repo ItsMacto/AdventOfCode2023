100b59e812ae76634f1cce53697d5bf6cf8303b7
Mac Howe <mac@machowe.com>
2023-12-08 16:11:57 -0500

    p2 wrong answer, 33102965 too low

--- day3/day3.py
+++ day3/day3.py
@@ -35,6 +35,60 @@ def star1():
                 x += 1
     return sum
                 
+                
+def checkGear(x, y, l, grid):
+    directions = [(-1, -1), (-1, 0), (-1, 1), (1, -1), (1, 0), (1, 1),(0,1),(0,-1)]
+    gear = ()
+    for i in range(l):
+        xOffset = x + i
+        for direction in directions:
+            dx, dy = direction
+            cx, cy = dx + xOffset, dy + y
+            
+            if (0<= cy < len(grid) and 0 <= cx < len(grid[cy])):
+                if (grid[cy][cx] == '*'):
+                    if gear != ():
+                        return
+                    else:
+                        gear = (cx, cy)
+    return gear
+        
+        
+def star2():
+    sum = 0
+    grid = []
+    gearMap = {}
+    with open('input.txt') as fp:
+        for line in fp:
+            grid.append(list(line.strip()))
+            
+        for y, line in enumerate(grid):
+            x = 0
+            while x < len(line):
+                if line[x].isnumeric():
+                    num = ''
+                    start_x = x
+                    while x < len(line) and line[x].isnumeric():
+                        num += line[x]
+                        x += 1
+                    l = len(num) 
+                    gear = checkGear(start_x, y, l, grid)
+                    print(gear)
+                    if gear != ():
+                        if gear not in gearMap:
+                            gearMap[gear] = []
+                        gearMap[gear].append(num)
+                        
+                    
+                        
+                        
+                x += 1
+        for gears in gearMap.values():
+            if len(gears) == 2:
+                sum += (int(gears[0]) * int(gears[1]))
+    return sum
+                     
+                
 
 
-print(star1())
+print(star2())
